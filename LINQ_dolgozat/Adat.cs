e4bfd91ec1c9b19a089e9e94b82814317d94fe50
Szima Ákos <indoorkechup@gmail.com>
2023-05-31 09:33:27 +0200

    felido

---
+++ LINQ_dolgozat/Adat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_dolgozat
+{
+    class Adat
+    {
+        public string Ev;
+        public string Nev;
+        public string Vegyjel;
+        public int Rendszam;
+        public string Felfedezo;
+
+        public Adat(string s){
+            string[] darabok = s.Split(';');
+            this.Ev = darabok[0];
+            this.Nev = darabok[1];
+            this.Vegyjel = darabok[2];
+            this.Rendszam = Convert.ToInt32(darabok[3]);
+            this.Felfedezo = darabok[4];
+        
+                }
+
+    }
+}
--- LINQ_dolgozat/Program.cs
+++ LINQ_dolgozat/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace LINQ_dolgozat
 {
@@ -10,6 +11,49 @@ namespace LINQ_dolgozat
     {
         static void Main(string[] args)
         {
+            List<Adat> lista = new List<Adat>();
+
+            using (StreamReader sr = new StreamReader("felfedezesek.csv"))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string sor = sr.ReadLine();
+                    Adat s = new Adat(sor);
+                    lista.Add(s);
+                }
+            }
+
+            Console.WriteLine($"3. feladat: Elemek száma: {lista.Count}");
+
+            int okor = lista.Where(x=>x.Ev == "Ókor").Count();
+            Console.WriteLine($"4. feladat: Felfedezések száma az ókorban: {okor}");
+
+            string betuk = Console.ReadLine();
+
+            while (betuk.Length == 2 || betuk.Length == 1 && )
+            {
+
+            }
+
+            Console.WriteLine("6. feladat: Keresés");
+            foreach (var item in lista.Where(x => x.Vegyjel.Contains(betuk)))
+            {
+                Console.WriteLine($"\tAz elem vegyjele: {item.Vegyjel}");
+
+                Console.WriteLine($"\tAz elem neve: {item.Nev}");
+
+                Console.WriteLine($"\tRendszáma: {item.Rendszam}");
+
+                Console.WriteLine($"\tFelfedezés éve: {item.Ev}");
+
+                Console.WriteLine($"\tFelfedező: {item.Felfedezo}");
+            }
+
+
+
+            int legkisebb = lista.Where(x=> x.Ev.Contains)
+            Console.WriteLine($"7. feladat: {} év volt a leghosszabb idő két elem felfedezése között.");
         }
     }
 }
