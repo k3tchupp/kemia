eb7d3e07d537b80a14b0ead819fec3f7f0089b36
tanulo <tanulo@CAD2-11>
2023-05-31 08:58:22 +0200

    Add project files.

---
+++ LINQ_dolgozat/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_dolgozat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
